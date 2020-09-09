32bdc1e56fa8fd5aa98205203c657fdd2c5f5cef
Nestor <nestor.marcano6>
2020-09-09 12:28:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState }from 'react';
 import './App.css';
+import ExpenseList from './components/ExpenseList';
+import ExpenseForm from './components/ExpenseForm';
+import Alert from './components/Alert';
+import uuid from 'uuid/v4';
+
+const initialExpenses = [
+  {id: uuid(), charge: 'rent', amount: 1600},
+  {id: uuid(), charge: 'car payment', amount: 400},
+  {id: uuid(), charge: 'credit card bill', amount: 1200}
+];
 
 function App() {
+  
+  //***** state  values
+  //all expenses, add expense
+  const [ expenses, setExpenses ] = useState(initialExpenses)
+  //single expense
+  const [ charge, setCharge ] = useState('');
+  //single amount
+  const [ amount, setAmount ] = useState('');
+  //alert
+  const [ alert, setAlert ] = useState({show: false});
+  //edit
+  const [edit, setEdit ] = useState(false);
+  //edit item
+  const [ id, setId ] = useState(0);
+  
+  //***** functionality
+  const handleCharge = (e) => {
+    setCharge(e.target.value);
+  }
+  const handleAmount = (e) => {
+    setAmount(e.target.value);
+  }
+  const handleAlert = ({type, text}) => {
+    setAlert({ show:true, type, text })
+    setTimeout(()=>{
+      setAlert({show:false})
+    }, 3000)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (charge !== '' && amount > 0) {
+
+      if(edit){
+        let tempExpenses = expenses.map( item => {
+          return item.id === id ? {...item, charge, amount} : item
+          })
+        setExpenses(tempExpenses);
+        setEdit(false);
+        handleAlert({type:'success', text:'item edited'});
+      } else {
+        const singleExpense = {
+          id: uuid(),
+          charge: charge,
+          amount: amount
+        }
+        setExpenses([...expenses, singleExpense])
+        handleAlert({type:'success', text:'item added'});
+      }
+      setCharge('');
+      setAmount('');
+     
+    } else {
+      //handle alert
+      handleAlert({type: 'danger', text: `charge can´t be a empty value and amount value has to be bigger than zero`})
+    }
+  };
+  //clear all items
+  const clearItems = () => {
+    setExpenses([]);
+    handleAlert({ type:'danger', text:'all items deleted'})
+  }
+  //handle delete
+  const handleDelete = (id) =>{
+    const tempExpenses = expenses.filter( item => item.id !== id)
+    setExpenses(tempExpenses);
+    handleAlert({ type: 'danger', text: 'item deleted'})
+  }
+  //handle edit
+  const handleEdit = (id) => {
+    console.log(`item edited ${id}`)
+    let expense = expenses.find(item => item.id === id )
+    let {charge, amount} = expense;
+    setCharge(charge);
+    setAmount(amount);
+    setEdit(true);
+    setId(id);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {alert.show && <Alert type={alert.type} text={alert.text} />}
+      <Alert />
+      <h1>Budget Calculator</h1>
+      <main className='App'>
+        <ExpenseForm 
+          charge={charge}
+          amount={amount}
+          handleAmount={handleAmount}
+          handleCharge={handleCharge}
+          handleSubmit={handleSubmit}
+          edit={edit}
+        />
+        <ExpenseList 
+          expenses={expenses}
+          clearItems={clearItems}
+          handleDelete={handleDelete}
+          handleEdit={handleEdit}
+        /> 
+      </main>
+      <h1>
+        total spending : <span className='total'>
+          $ {expenses.reduce((acc, curr)=> {
+            return (acc += parseInt(curr.amount));
+          }, 0)}
+        </span>
+      </h1>
+    </>
   );
 }
 
